3e2c7be12d611ee9b5e8eb28d9b39ba2dbbc8fdb
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-04-09 19:09:02 +0300

    add debugging tools for article.test.js and Header.test.js

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -6,6 +6,10 @@ import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
+  
+  const element = screen.queryByText("hello from the Header!");
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  screen.debug();
+
+  expect(element).toBeInTheDocument();
 });
